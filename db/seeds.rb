ffe0da085b2c981d89c1d2ef55962a9aa6f89008
chels <chemccann@hotmail.com>
2019-11-05 17:55:54 -0500

    Seeded historical transactions

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,17 +10,37 @@ Ticker.destroy_all
 User.destroy_all
 Transaction.destroy_all
 
-User.create!(email: "JordanBelfort@w.com", password: "password123", buying_power: 1000000)
+user = User.create!(email: "JordanBelfort@w.com", password: "password123", buying_power: 1000000)
 
-# Ticker.create!(symbol: "AAPL", num_shares: 2) 
-# Ticker.create!(symbol: "FB", num_shares: 2)
-# Ticker.create!(symbol: "GOOG", num_shares: 1)
-# Ticker.create!(symbol: "NKE", num_shares: 12)
-# Ticker.create!(symbol: "RCL", num_shares: 5)
+ticker1 = Ticker.create!(symbol: "AAPL", num_shares: 2) 
+t1 = Transaction.new(ticker_symbol: "AAPL", purchase_price: 251.30, purchase_shares: 2, buy: true, ticker_id: ticker1.id, user_id: user.id)
+t1.update_attributes(created_at: "2019-11-01T12:18:49.616Z", updated_at: "2019-11-01T12:18:49.616Z")
+t1.save!
+
+
+ticker2 = Ticker.create!(symbol: "FB", num_shares: 2)
+t2 = Transaction.new(ticker_symbol: "FB", purchase_price: 251.30, purchase_shares: 2, buy: true, ticker_id: ticker2.id, user_id: user.id)
+t2.update_attributes(created_at: "2019-4-01T12:18:49.616Z", updated_at: "2019-4-01T12:18:49.616Z")
+t2.save!
+
+ticker3 = Ticker.create!(symbol: "GOOG", num_shares: 1)
+t3 = Transaction.new(ticker_symbol: "GOOG", purchase_price: 251.30, purchase_shares: 2, buy: true, ticker_id: ticker3.id, user_id: user.id)
+t3.update_attributes(created_at: "2019-7-01T12:18:49.616Z", updated_at: "2019-7-01T12:18:49.616Z")
+t3.save!
+
+ticker4 = Ticker.create!(symbol: "NKE", num_shares: 12)
+t4 = Transaction.new(ticker_symbol: "NKE", purchase_price: 251.30, purchase_shares: 2, buy: true, ticker_id: ticker4.id, user_id: user.id)
+t4.update_attributes(created_at: "2019-10-23T12:18:49.616Z", updated_at: "2019-10-23T12:18:49.616Z")
+t4.save!
+
+ticker5 = Ticker.create!(symbol: "RCL", num_shares: 5)
+t5 = Transaction.new(ticker_symbol: "RCL", purchase_price: 251.30, purchase_shares: 2, buy: true, ticker_id: ticker5.id, user_id: user.id)
+t5.update_attributes(created_at: "2019-11-04T12:18:49.616Z", updated_at: "2019-11-04T12:18:49.616Z")
+t5.save!
+
 # Ticker.create!(symbol: "CROX", num_shares: 2)
 # Ticker.create!(symbol: "MSFT", num_shares: 8)
 # Ticker.create!(symbol: "CRM", num_shares: 5)
 # Ticker.create!(symbol: "LYFT", num_shares: 3)
 # Ticker.create!(symbol: "OSTK", num_shares: 4)
 
-# Transaction.create!(ticker_symbol: "AAPL", purchase_price: 251.30, purchase_shares: 2, buy: true)
