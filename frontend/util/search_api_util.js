2869428e87e5f7e76beaae857e80b7c03e1aab7d
chels <chemccann@hotmail.com>
2019-11-14 09:25:07 -0500

    switched to test data

--- frontend/util/search_api_util.js
+++ frontend/util/search_api_util.js
@@ -1,8 +1,8 @@
 export const fetchFromAPI = (query) => {
     return $.ajax({
         method: "GET", 
-        // url: `https://sandbox.iexapis.com/stable/stock/${query}/quote?token=Tpk_4ca09027bbda4ce1a28d8e1702fafdaa`
-        url: `https://cloud.iexapis.com/stable/stock/${query}/quote?token=pk_b6f890a95fb24dbfb1a85f362fe5687f`
+        url: `https://sandbox.iexapis.com/stable/stock/${query}/quote?token=Tpk_4ca09027bbda4ce1a28d8e1702fafdaa`
+        // url: `https://cloud.iexapis.com/stable/stock/${query}/quote?token=pk_b6f890a95fb24dbfb1a85f362fe5687f`
     })
 }
 
@@ -10,8 +10,8 @@ export const fetchFromAPI = (query) => {
 export const fetchAllFromAPI = (query) => {
     return $.ajax({
         method: "GET",
-        // url: `https://sandbox.iexapis.com/stable/search/${query}?token=Tpk_4ca09027bbda4ce1a28d8e1702fafdaa`
-        url: `https://cloud.iexapis.com/stable/search/${query}?token=pk_b6f890a95fb24dbfb1a85f362fe5687f`
+        url: `https://sandbox.iexapis.com/stable/search/${query}?token=Tpk_4ca09027bbda4ce1a28d8e1702fafdaa`
+        // url: `https://cloud.iexapis.com/stable/search/${query}?token=pk_b6f890a95fb24dbfb1a85f362fe5687f`
 
         // url: `https://sandbox.iexapis.com/stable/ref-data/symbols?token=Tpk_4ca09027bbda4ce1a28d8e1702fafdaa`
         // url: `https://sandbox.iexapis.com/stable/ref-data/iex/symbols?token=Tpk_4ca09027bbda4ce1a28d8e1702fafdaa`
