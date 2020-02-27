a31a4832838e791981d03860d0013fb5be89d077
chels <chemccann@hotmail.com>
2020-02-26 23:11:19 -0500

    Cleaned up app.jsx

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import DashboardContainer from './dashboard/dashboard_container';
 import GreetingContainer from './greeting/greeting_container';
 import LoginFormContainer from './login_form/login_form_container';
 import SignupFormContainer from './signup_form/signup_form_container';
-import TickerShowContainer from './ticker/ticker_show_container';
-import { Route, Switch } from 'react-router-dom';
-import { AuthRoute, ProtectedRoute } from '../util/route_utils';
-import Splash from './splash/splash';
+import { Switch } from 'react-router-dom';
+import { AuthRoute } from '../util/route_utils';
 
 // protected route to be added once addtional pages added as comps
 // greeting container does not need protected route since conditional checks for loggedin/loggedout and renders based on that
@@ -17,9 +14,6 @@ const App = () => (
       <GreetingContainer />
     </header>
     <Switch>
-      {/* <ProtectedRoute exact path="/" component={DashboardContainer}/> */}
-      {/* <AuthRoute exact path="/" component={Splash}/> */}
-      {/* <ProtectedRoute exact path='/:tickerSymbol' component={TickerShowContainer}/> */}
       <AuthRoute path="/login" component={LoginFormContainer} />
       <AuthRoute path="/signup" component={SignupFormContainer} />
     </Switch>
